0b83142fd9e720acb58d3512877c80a8f221b91c
heineken <lenguyenquanduy@gmail.com>
2025-03-24 18:47:34 +0700

    Implement SQL query building: update build_sql_query and visit_parse_tree functions to tokenize input and generate raw SQL queries

--- src/application/build_sql_query.py
+++ src/application/build_sql_query.py
@@ -1,7 +1,9 @@
+from src.application import parse_tokens, tokenize_input, visit_parse_tree
 from src.models.queries_factory.query_types import QueryType
 
-def build_sql_query(extracted_data: dict, type: QueryType) -> str:
-    """
-    Select the right Builder by QueryType and convert extracted data into raw SQL query
-    """
-    pass
+def build_sql_query(sqn: str, type: QueryType) -> str:
+    tokens = tokenize_input(sqn, type);
+    tree = parse_tokens(tokens, type);
+    raw_sql_query = visit_parse_tree(tree, type);
+
+    return raw_sql_query;
